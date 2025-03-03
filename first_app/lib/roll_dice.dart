f1b47fb59e7ac59bfe0de1137303a117dd0de350
dag14 <dagimasrat3@gmail.com>
2025-03-03 23:35:31 +0300

    add random numbers to dice rolls

--- first_app/lib/roll_dice.dart
+++ first_app/lib/roll_dice.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -9,10 +12,10 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-1.png';
+  var currentDiceRoll = 2;
   void rollDice() {
     setState(() {
-      activeDiceImage = 'assets/images/dice-2.png';
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
   }
 
@@ -21,7 +24,7 @@ class _DiceRollerState extends State<DiceRoller> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Image.asset(activeDiceImage, width: 200),
+        Image.asset('assets/images/dice-$currentDiceRoll.png', width: 200),
         SizedBox(height: 20),
         TextButton(
           onPressed: rollDice,
