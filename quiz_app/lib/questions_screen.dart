65393fc5309be13eaf1ff376e833638282ad8431
dag14 <dagimasrat3@gmail.com>
2025-03-07 19:58:36 +0300

    make the questions and answers object with UI

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,6 +12,18 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return Text("data");
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(style: TextStyle(color: Colors.white), 'This is the question'),
+          const SizedBox(height: 30),
+          AnswerButton(answerText: 'Answer 1', onTap: () {}),
+          AnswerButton(answerText: 'Answer 2', onTap: () {}),
+          AnswerButton(answerText: 'Answer 3', onTap: () {}),
+        ],
+      ),
+    );
   }
 }
