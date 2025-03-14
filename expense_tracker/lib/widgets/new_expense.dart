af54ef4a4358e8b8b4a74409aa6916a0e3e6b1d4
dag14 <dagimasrat3@gmail.com>
2025-03-14 19:29:38 +0300

    add modal sheet and accept user inputs of title and amount

---
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: InputDecoration(label: Text("Title")),
+          ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              prefixText: '\$ ',
+              label: Text("Amount"),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: const Text("Save Expense!"),
+              ),
+              TextButton(onPressed: () {}, child: const Text("Cancel")),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
