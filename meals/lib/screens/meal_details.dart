b135066ee9b3ec6d1441f5ab6afc5ed6edbeff20
dag14 <dagimasrat3@gmail.com>
2025-04-28 19:43:37 +0300

    implement riverpod for app wode state management

--- meals/lib/screens/meal_details.dart
+++ meals/lib/screens/meal_details.dart
@@ -10,6 +10,9 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favouriteMeals = ref.watch(favouriteMealsProvider);
+
+    final isFavourite = favouriteMeals.contains(meal);
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -30,7 +33,7 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: const Icon(Icons.star),
+            icon: Icon(isFavourite ? Icons.star : Icons.star_border),
           ),
         ],
       ),
